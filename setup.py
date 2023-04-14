196530fba562f70dfa77add2d86290e396c7452e
Silvio Traversaro <silvio@traversaro.it>
2023-04-14 09:51:01 +0200

    Add GitHub Action to test Python package

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ requirements = [
     'scipy',            # For trimesh, annoyingly
     'six',              # Python 2/3 compatability
     'trimesh',          # Mesh geometry loading/creation/saving
+    'wheel',            # For setup
 ]
 
 dev_requirements = [
